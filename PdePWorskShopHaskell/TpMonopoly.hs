510c0f1db97c7b0dcffd2afa1fdc6ab6107ed67f
napetruzzi <npetruzzi@frba.utn.edu.ar>
2021-05-13 20:42:48 -0300

    Arregle repeticion de logica y elimine comentarios

--- PdePWorskShopHaskell/TpMonopoly.hs
+++ PdePWorskShopHaskell/TpMonopoly.hs
@@ -6,19 +6,28 @@ type Propiedades = (String,Int)
 type Dinero = Int
 type Accion = Persona -> Persona
 type Ganador = String
--- UN TYPE ALIAS GANADADOR???
+
 
 data Persona = UnJugador{
     nombre                      :: String,
-    cantidadDinero              :: Int, --empeizan con 500
+    cantidadDinero              :: Int,
     tacticaDeJuego              :: String,
-    propiedadesCompradas        ::  [Propiedades], --una tupla con Nombre y precio
+    propiedadesCompradas        ::  [Propiedades],
     accionesDuranteElJuego      :: [Accion]
 }deriving (Show)
 
 
+--Ejemplos de Propiedades:
+nuevaYork :: Propiedades
+nuevaYork = ("Ny", 350)
+california :: Propiedades
+california = ("Cali",250)
+texas :: Propiedades
+texas = ("Tx",100)
+
+
 carolina :: Persona
-carolina  =  (UnJugador "Carolina" 500 "Accionista" [("Ny",10),("Cali",160)] [pasarPorElBanco,pagarAAccionistas])
+carolina  =  (UnJugador "Carolina" 500 "Accionista" [] [pasarPorElBanco,pagarAAccionistas])
 manuel :: Persona
 manuel = (UnJugador "Manuel" 500 "Oferente singular" [] [pasarPorElBanco,enojarse])
 
@@ -65,14 +74,14 @@ propiedadesACobrar:: (Int -> Bool) -> Persona -> Int
 propiedadesACobrar funcion unjugador =  length(filter funcion (map snd (propiedadesCompradas unjugador)))
 
 cobrarAlquileres:: Persona -> Persona
-cobrarAlquileres unjugador =  modificarDinero ((propiedadesACobrar (150<=) unjugador)*20 + (propiedadesACobrar (150>) unjugador)*10) unjugador --unjugador{cantidadDinero = cantidadDinero unjugador + (propiedadesACobrar (150<=) unjugador)*20 + (propiedadesACobrar (150>) unjugador)*10 } 
+cobrarAlquileres unjugador =  modificarDinero ((propiedadesACobrar (150<=) unjugador)*20 + (propiedadesACobrar (150>) unjugador)*10) unjugador  
 
 
 --pagarAAccionistas: 
 pagarAAccionistas:: Persona -> Persona
 pagarAAccionistas unjugador
- |tieneTacticaAccionista unjugador = (modificarDinero 200 unjugador)-- unjugador{cantidadDinero = cantidadDinero unjugador +200}
- |otherwise =  (modificarDinero (-100) unjugador) --unjugador{cantidadDinero = cantidadDinero unjugador - 100}
+ |tieneTacticaAccionista unjugador = (modificarDinero 200 unjugador)
+ |otherwise =  (modificarDinero (-100) unjugador) 
 
 
 --hacerBerrinchePor: 
@@ -80,7 +89,6 @@ hacerBerrinchePor :: (String,Int) -> Persona -> Persona
 hacerBerrinchePor propiedad unjugador 
  | tieneDinero propiedad unjugador = comprarPropiedad propiedad unjugador
  | otherwise = (hacerBerrinchePor propiedad).gritar.(modificarDinero 10) $ unjugador
-
 
 
 --juegoFinal
@@ -92,14 +100,3 @@ juegoFinal jugador1 jugador2
  | (cantidadDinero ((ultimaRonda jugador1) jugador1)) > (cantidadDinero ((ultimaRonda jugador2) jugador2)) = (nombre jugador1)
  | otherwise = nombre jugador2
 
-
-{-
-
-Se pide:
-Modelar a Carolina y Manuel.
-Modelar las acciones.
-Modelar las propiedades.
-Hacer una función juegoFinal la cual toma dos participantes y devuelve al ganador.
-
-
--}
